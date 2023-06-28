84e29861cf30b6d75235093ce8f8f104009c7623
comeP <come.picard@gmail.com>
2023-06-28 22:16:51 +0200

    :sparkles: FEAT buttons actions:
    ajout d'actions sur les boutons

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendValue(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    try {
+        const result = eval(document.getElementById("result").value);
+        document.getElementById("result").value = result;
+    } catch (error) {
+        document.getElementById("result").value = "Error";
+    }
+}
+
+function clearResult() {
+    document.getElementById("result").value = "";
+}
+
+function clearError() {
+    document.getElementById("result").value = "";
+}
+
