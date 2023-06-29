a287296dc5802a2fa632b5a7a60132feee4f8487
comeP <come.picard@gmail.com>
2023-06-29 22:40:59 +0200

    :sparkles: FEAT history actions:
    Ajout du nettoyage de l'historique et de l'export au format CSV

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let history = [];
+
 function appendValue(value) {
     document.getElementById("result").value += value;
 }
@@ -9,8 +11,9 @@ function calculate() {
         document.getElementById("result").value = result;
 
         let time = new Date();
+        let formattedTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds() > 9 ? time.getSeconds() : '0'+time.getSeconds()}`
         let expression_time = document.createElement("p");
-        expression_time.textContent = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+        expression_time.textContent = formattedTime;
 
         let historyContainer = document.getElementById("historyContainer");
         let history_expression = document.createElement("p");
@@ -30,6 +33,9 @@ function calculate() {
         historyContainer.appendChild(history_result);
 
         historyContainer.scrollTop = historyContainer.scrollHeight;
+
+        let history_line = [expression, result, formattedTime]
+        history.push(history_line)
     } catch (error) {
         document.getElementById("result").value = "Error";
     }
@@ -41,6 +47,34 @@ function clearResult() {
 
 function clearError() {
     document.getElementById("result").value = "";
+}
+
+function clearHistory(){
+    let history_to_clear = document.getElementById("historyContainer");
+    history = [];
+    while(history_to_clear.firstChild){
+        history_to_clear.removeChild(history_to_clear.firstChild);
+    }
+}
+
+function exportHistory(){
+    let time = new Date();
+    let formattedTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds() > 9 ? time.getSeconds() : '0'+time.getSeconds()}`
+
+    let csvContent =
+        "data:text/csv;charset=utf-8,"
+        + 'Expression :, Result :, Time : \n'
+        + history.map(e => e.join(",")).join("\n");
+
+    console.log(csvContent)
+
+    let encodedUri = encodeURI(csvContent);
+    let link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", `calculator_export_${formattedTime}.csv`);
+    document.body.appendChild(link);
+
+    link.click();
 }
 
 document.addEventListener("keypress", function(event) {
@@ -81,3 +115,4 @@ document.addEventListener("keyup", function(event) {
         button.classList.remove('active');
     }
 })
+
