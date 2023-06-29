cb74cd31c8d9112bd9bfa7efa4d74d96c3f0e6be
comeP <come.picard@gmail.com>
2023-06-29 19:34:23 +0200

    :sparkles: FEAT Add history:
    ajout de l'historique de la gitculette

--- script.js
+++ script.js
@@ -4,8 +4,32 @@ function appendValue(value) {
 
 function calculate() {
     try {
-        const result = eval(document.getElementById("result").value);
+        const expression = document.getElementById("result").value;
+        const result = eval(expression);
         document.getElementById("result").value = result;
+
+        let time = new Date();
+        let expression_time = document.createElement("p");
+        expression_time.textContent = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+
+        let historyContainer = document.getElementById("historyContainer");
+        let history_expression = document.createElement("p");
+        history_expression.classList.add("calcul_expression");
+        history_expression.textContent = expression;
+
+        let history_result = document.createElement("p");
+        history_result.classList.add("calcul_result");
+        history_result.textContent = `> ${result}`;
+
+        let expression_line = document.createElement("div")
+        expression_line.classList.add("calcul_expression_line");
+
+        expression_line.appendChild(history_expression);
+        expression_line.appendChild(expression_time);
+        historyContainer.appendChild(expression_line);
+        historyContainer.appendChild(history_result);
+
+        historyContainer.scrollTop = historyContainer.scrollHeight;
     } catch (error) {
         document.getElementById("result").value = "Error";
     }
@@ -30,16 +54,15 @@ document.addEventListener("keypress", function(event) {
 
 document.addEventListener("keydown", function(event) {
     const keyPressed = event.key;
-    console.log(keyPressed)
     if(/[0-9\/\*\-\+\.]/.test(keyPressed)){
-        var button = document.getElementById(`key_${keyPressed}`)
+        let button = document.getElementById(`key_${keyPressed}`)
         button.classList.add('active');
     } else if(keyPressed==="Escape" || keyPressed==="Backspace"){
-        var button = document.getElementById(`key_clear`)
+        let button = document.getElementById(`key_clear`)
         button.classList.add('active');
         clearResult()
     } else if(keyPressed==="Enter"){
-        var button = document.getElementById(`key_=`)
+        let button = document.getElementById(`key_=`)
         button.classList.add('active');
         calculate()
     }
@@ -48,14 +71,13 @@ document.addEventListener("keydown", function(event) {
 document.addEventListener("keyup", function(event) {
     const keyPressed = event.key;
     if(/[0-9\/\*\-\+\.]/.test(keyPressed)){
-        var button = document.getElementById(`key_${keyPressed}`)
+        let button = document.getElementById(`key_${keyPressed}`)
         button.classList.remove('active');
     } else if(keyPressed==="Escape" || keyPressed==="Backspace"){
-        var button = document.getElementById(`key_clear`)
+        let button = document.getElementById(`key_clear`)
         button.classList.remove('active');
     } else if(keyPressed==="Enter"){
-        var button = document.getElementById(`key_=`)
+        let button = document.getElementById(`key_=`)
         button.classList.remove('active');
-        calculate()
     }
 })
